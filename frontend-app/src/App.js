5162f3a5e003c159f3836e19885245bd777ef4aa
Evan Morris <evanmorris97@gmail.com>
2021-03-09 12:44:42 +0000

    login component and router
    Make sure you npm install before making any new changes

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import './App.css';
 import Navbar from "./components/navbar";
+import LogIn from "./components/login";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Navbar />
-      </div>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path = "/" component = {Home} />
+            <Route exact path = "/login" component = {LogIn} />
+          </Switch>
+        </BrowserRouter> 
 
+
+      </div>
     </div>
   );
 }
