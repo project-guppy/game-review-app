32810b9c7d2591741d9d794176a9c0bc38b2f94b
Evan Morris <evanmorris97@gmail.com>
2021-03-09 14:08:52 +0000

    signup page and link and route

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -4,15 +4,20 @@ import './App.css';
 import Navbar from "./components/navbar";
 import LogIn from "./components/login";
 import Home from "./components/home";
+import SignUp from "./components/signup";
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
-          <Navbar />
+          <div>
+            <Navbar />
+          </div>
+          
           <Switch>
             <Route exact path = "/" component = {Home} />
             <Route exact path = "/login" component = {LogIn} />
+            <Route exact path = "/signup" component = {SignUp} />
           </Switch>
         </BrowserRouter>     
     </div>
