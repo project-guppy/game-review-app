f4f703aa7e1541dc7229000b18a75269a9e1e569
Evan Morris <evanmorris97@gmail.com>
2021-03-09 13:35:32 +0000

    fix - move grid to Home component

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -4,26 +4,17 @@ import './App.css';
 import Navbar from "./components/navbar";
 import LogIn from "./components/login";
 import Home from "./components/home";
-import Grid from "./components/grid";
 
 function App() {
   return (
     <div className="App">
-      <div>
         <BrowserRouter>
           <Navbar />
           <Switch>
             <Route exact path = "/" component = {Home} />
             <Route exact path = "/login" component = {LogIn} />
           </Switch>
-        </BrowserRouter> 
-    
-      <div className="navigationBar">
-        <Navbar />
-      </div>
-      <div className="grid">
-        <Grid />
-      </div>
+        </BrowserRouter>     
     </div>
   );
 }
