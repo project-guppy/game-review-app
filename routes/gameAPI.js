4d33cf47426f7dfe6e9d33154bb93220790ddfc0
Stephen Booth <stephenboothdev@gmail.com>
2021-03-10 13:45:38 +0000

    limit games returned to 20

--- routes/gameAPI.js
+++ routes/gameAPI.js
@@ -3,13 +3,18 @@ const gameApi = express.Router();
 
 const Game = require("../models/games");
 
-gameApi.get("/", (req, res) => {
+gameApi.get("/", async (req, res) => {
   console.log(req.query);
-  Game.find(req.query, (err, obj) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(obj));
-    res.send();
-  });
+  await Game.find(
+    req.query,
+    null,
+    { sort: { rating: -1 }, limit: 20 },
+    (err, obj) => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(obj));
+      res.send();
+    }
+  );
 });
 
 module.exports = gameApi;
