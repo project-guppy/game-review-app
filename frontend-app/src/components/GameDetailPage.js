b36c0ed8dbd4442311c71e1b7ede6b33a241884c
Stephen Booth <stephenboothdev@gmail.com>
2021-03-11 10:25:13 +0000

    Added game detail card

--- frontend-app/src/components/GameDetailPage.js
+++ frontend-app/src/components/GameDetailPage.js
@@ -2,22 +2,33 @@ import React from "react";
 
 import { Card, Image, Descriptions, Rate } from "antd";
 
+import "./GameDetailPage.css";
+
 const GameDetailPage = ({ game }) => {
   return (
-    <div className="gameDetailWrapper">
-      <Card title={game.name}>
-        <Image src={game.cover} width={200} />
-        <Rate disabled defaultValue={game.rating / 20} />
-        <Descriptions>
-          <Descriptions.Item label="Release Date">
-            {game.release_dates[0].human}
-          </Descriptions.Item>
-          <Descriptions.Item label="Genres">
-            {game.genres.join(", ")}
-          </Descriptions.Item>
-        </Descriptions>
-        <p>{game.summary}</p>
+    <div className="gameDetailPageWrapper">
+      <Card className>
+        <div className="gameDetailWrapper">
+          <div className="gameDetailSide">
+            <Image src={game.cover} width={200} />
+          </div>
+          <div className="gameDetailContent">
+            <h1>{game.name}</h1>
+            <Rate disabled defaultValue={game.rating / 20} />
+            <Descriptions>
+              <Descriptions.Item label="Release Date">
+                {game.release_dates[0].human}
+              </Descriptions.Item>
+              <Descriptions.Item label="Genres">
+                {game.genres.join(", ")}
+              </Descriptions.Item>
+            </Descriptions>
+            <p>{game.summary}</p>
+          </div>
+        </div>
       </Card>
     </div>
   );
 };
+
+export default GameDetailPage;
