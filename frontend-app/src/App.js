ab286cca6db43d5eb804f7f7aa548e3b21b1900f
Stephen Booth <stephenboothdev@gmail.com>
2021-03-11 10:25:13 +0000

    Added game detail card

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import LoginPage from "./components/LoginPage";
 import Home from "./components/Home";
 import SignUp from "./components/SignUpPage";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import GameDetailPage from "./components/GameDetailPage";
+
+import testGame from "./testGame.json";
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <div className="appNavbar">
-            <Navbar />
-          </div>
-                  
-          <Switch>
-            <Route exact path = "/" component = {Home} />
-            <Route exact path = "/login" component = {LoginPage} />
-            <Route exact path = "/signup" component = {SignUp} />
-            <Route exact path = "/about" component = {About} />
-          </Switch>
+      <BrowserRouter>
+        <div className="appNavbar">
+          <Navbar />
+        </div>
 
-          <div>
-            <Footer />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>  
-          </div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/detail">
+            <GameDetailPage game={testGame} />
+          </Route>
+        </Switch>
 
-        </BrowserRouter>     
+        <div>
+          <Footer />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+          ></link>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
