c2580a036e5a592c1b49b722a96a043796027c0b
Adam Manley <adam.p.manley@gmail.com>
2021-03-11 14:15:52 +0000

    Crards Added,
    Small css fixes

--- frontend-app/src/components/Footer.js
+++ frontend-app/src/components/Footer.js
@@ -4,9 +4,9 @@ const FooterComp = () => {
     return (
         <div className="footerWrapper">
             <div className="footerGits">
-                <a href="https://github.com/evanmorrisdev" class="fa fa-github" alt="Evan">Evan</a>
-                <a href="https://github.com/ap42m" class="fa fa-github" alt="Adam">Adam</a>
-                <a href="https://github.com/boothscript" class="fa fa-github" alt="Steve">Steve</a>
+                <a href="https://github.com/evanmorrisdev" className="fa fa-github" alt="Evan">Evan</a>
+                <a href="https://github.com/ap42m" className="fa fa-github" alt="Adam">Adam</a>
+                <a href="https://github.com/boothscript" className="fa fa-github" alt="Steve">Steve</a>
             </div>
         </div>
     )
