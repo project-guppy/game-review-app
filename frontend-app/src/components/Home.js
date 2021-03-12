33da50a2898d9c1cc83c2cfa42d3790b8b3fdd1d
Stephen Booth <stephenboothdev@gmail.com>
2021-03-12 13:00:29 +0000

    hompage now links to detail page

--- frontend-app/src/components/Home.js
+++ frontend-app/src/components/Home.js
@@ -1,28 +1,30 @@
 import GuppyCard from "./Card";
-import { useEffect, useState } from 'react';
-import './Home.css'
-
-
+import { useEffect, useState } from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    
-    const [games, setGames] = useState([])
-    useEffect(async() => {
-        const data = await fetch("http://localhost:3003/api/v1/games").then((res) =>res.json() )
-        console.log()
-        setGames(data)
-    }, [])
-    const cards = games.map((game) => {
-        return (<GuppyCard key={game.id} game={ game } />
-        )
-    });
-    console.log(cards)
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    const getGames = async () => {
+      const data = await fetch(
+        "http://localhost:3003/api/v1/games"
+      ).then((res) => res.json());
+      console.log();
+      setGames(data);
+    };
+    getGames();
+  }, []);
 
+  const cards = games.map((game) => {
     return (
- 
-        <div className="cardWrapper">
-            {cards}
-        </div>
-    )
-}
+      <Link to={`/game/${game.id}`}>
+        <GuppyCard key={game.id} game={game} />
+      </Link>
+    );
+  });
+  console.log(cards);
+
+  return <div className="cardWrapper">{cards}</div>;
+};
 export default HomePage;
